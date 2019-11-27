f596ace4143b88a63c29b60d33ce61dd349ffbdd
ADAM VARDEN <G00359605@gmit.ie>
2019-11-27 22:18:39 +0000

    Added Css

--- src/Components/home.js
+++ src/Components/home.js
@@ -2,10 +2,18 @@ import React from 'react';
 import '../App.css';
 
 class Home extends React.Component {
- render(){
+  render() {
     return (
       <div className="App">
-      <h1 >My Home in another component</h1>
+        <h1 >Welcome to the Employees Database</h1>
+        <p>
+          Here you find all information about all Employees.
+          <br></br>
+          In the Employees Links you will find links to view all employees,
+          <br></br>          
+          where you have the option to add, delete and update the employees within the database.
+          <br></br>
+        </p>
       </div>
     );
   }
